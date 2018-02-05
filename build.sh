6a478b7632bfb8c7493668830d2c3c630cd7c173
Devesh <deveshsinghal22@gmail.com>
2018-02-05 17:26:35 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 cd client;
+// npm install
 ng build --prod
 cd ..
 cd server
+// npm install
+export NODE_ENV=development
+npm start
--- client/src/app/models/event.ts
+++ client/src/app/models/event.ts
@@ -1,5 +1,6 @@
 export interface IEvent {
   name: string;
   slug: string;
+  isMember: boolean;
   description: string;
 }
