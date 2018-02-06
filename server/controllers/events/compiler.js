75bc9f908e491b8c5207f16e42f18618a39d66c1
Akash Yadav <akash.yadav@accoliteindia.com>
2018-02-07 00:39:31 +0530

    Added java,python and php

--- server/controllers/events/compiler.js
+++ server/controllers/events/compiler.js
@@ -4,19 +4,36 @@ var User = require('../../models/User');
 var router = express.Router();
 var async = require('async');
 var request = require('request');
-
+var langObj = {
+    java: {
+        name: 'java',
+        ext: 'java'
+    },
+    php: {
+        name: 'php',
+        ext: 'php'
+    },
+    python: {
+        name: 'python',
+        ext: 'py'
+    }
+}
 router.post('/run', function (req, res, next) {
+
     console.log("in the unnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn")
+
     var code = req.body.code.replace(/(\n\t|\n|\t)/gm," ");
+    var language= req.body.language;
+    var lang = langObj[language];
     console.log(code);
     var options = {
         method: 'POST',
-        url: 'https://run.glot.io/languages/java/latest',
+        url: 'https://run.glot.io/languages/'+lang.name+'/latest',
         headers: {
             'Authorization': 'Token 9b90f202-6a45-451d-bc8f-7a72707242c0',
             'Content-type': 'application/json'
         },
-        json: {"files": [{"name": "main.java", "content": code
+        json: {"files": [{"name": "main."+lang.ext , "content": code
             }]
         }
     }
@@ -32,6 +49,7 @@ router.post('/run', function (req, res, next) {
 
             //console.log('success');
             //console.log(response.body);
+
             res.json(response.body);
         }
     }
