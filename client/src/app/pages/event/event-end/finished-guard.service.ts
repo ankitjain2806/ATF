a4bfae4e220a8a98535e14b7fcfcf46e4d85dc14
AravindM-Accolite <aravindkumar.metku@github.com>
2018-02-08 15:27:58 +0530

    added finished guard service

---
+++ client/src/app/pages/event/event-end/finished-guard.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from "@angular/router";
+import {Observable} from "rxjs/Observable";
+import {EventService} from "../event.service";
+import {UserSessionService} from "../../../shared/util/user-session.service";
+
+@Injectable()
+export class FinishedGuardService implements CanActivate {
+
+  private route;
+  private state;
+
+  constructor(
+    private eventService: EventService,
+    private userSession: UserSessionService) { }
+
+  canActivate(route: ActivatedRouteSnapshot,
+              state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    this.route = route;
+    this.state = state;
+    return this.checkIfUserActuallyFinished().map(e => {
+      return e['data'];
+    });
+  }
+
+  private checkIfUserActuallyFinished(): Observable<boolean> {
+    return this.eventService.isEventEnded(this.route.params['slug'], this.userSession.getSession()._id);
+  }
+}
