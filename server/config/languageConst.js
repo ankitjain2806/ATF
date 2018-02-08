e0cf3c0fa45c6e2c2b2f50721697a1a2746a8cc3
Akash Yadav <akash.yadav@accoliteindia.com>
2018-02-09 01:16:00 +0530

    Adding more languages to compiler

--- server/config/languageConst.js
+++ server/config/languageConst.js
@@ -1,16 +1,38 @@
 var langObj = {
+  c: {
+    name: 'c',
+    ext: 'c'
+  },
+  cpp: {
+    name: 'c++',
+    ext: 'cpp'
+  },
   java: {
     name: 'java',
     ext: 'java'
+  },
+  javascript: {
+    name: 'javascript',
+    ext: 'js'
   },
   php: {
     name: 'php',
     ext: 'php'
   },
+  perl: {
+    name: 'perl',
+    ext: 'pl'
+  },
   python: {
     name: 'python',
     ext: 'py'
+  },
+  ruby: {
+    name: 'ruby',
+    ext: 'rb'
   }
 };
 
 module.exports = langObj;
+
+
