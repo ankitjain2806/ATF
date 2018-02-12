77bbe05569c410a9d16a811f680bbb54734308b1
root <root@ip-172-31-15-250.ap-southeast-1.compute.internal>
2018-02-12 10:58:25 +0000

    Updated the script

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
+
+echo "Fetching updates from repository"
 git pull
 cd client
+echo "Updating client npm"
 npm install
+echo "Building code for production"
 ng build --prod
 cd ..
 cd server
+echo "Updating server npm"
 npm install
 export NODE_ENV=development
+echo "Starting the server"
 #npm start
 nohup  npm start & > log1.js
+
 
