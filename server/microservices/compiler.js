5b5dde111aa403aaa350d88d905727338f420c5a
Akash Yadav <akash.yadav@accoliteindia.com>
2018-02-18 23:34:59 +0530

    Added test case checking in compiler.js

--- server/microservices/compiler.js
+++ server/microservices/compiler.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
 var amqp = require('amqplib/callback_api');
+var async = require('async');
 var request = require('request')
 amqp.connect('amqp://localhost:5672', function (err, conn) {
   // console.log(err,conn)
@@ -37,10 +38,87 @@ amqp.connect('amqp://localhost:5672', function (err, conn) {
           resultObj.matching=true;
         }
         // ch.assertQueue(resultQueue, {durable: false});
-        ch.sendToQueue(resultQueue, new Buffer(JSON.stringify(resultObj)));
+          ch.sendToQueue(resultQueue, new Buffer(JSON.stringify(resultObj)));
       }
+      var i;
+      var a=[];
+      for(i=0;i<obj.testCases.testCases.length;i++){
+        a[i]=function(i) {
+            options.json.stdin=obj.testCases.testCases[i].stdin;
+            console.log("Test1 in loop" + obj.testCases.testCases[i].stdin);
+            request(options, function(err, res, body) {
+                var resultObj = {
+                    error: err,
+                    response: body,
+                    status: res.statusCode,
+                    matching:false
+                };
+                if(obj.testCases.testCases[i].stdout == body.stdout){
+                    resultObj.matching=true;
+                    console.log("Test "+i+" pass:");
+                }
+                console.log("resulta : "+  JSON.stringify(resultObj));
+                //send to queue obj.testCases.testCases[0].stdout
+            });
+        }
+      }
+      console.log("value of a"+ a);
+      async.parallel([
+            function() {
+              options.json.stdin=obj.testCases.testCases[0].stdin;
+              console.log("Test1" + obj.testCases.testCases[0].stdin);
+              request(options, function(err, res, body) {
+                var resultObj = {
+                  error: err,
+                  response: body,
+                  status: res.statusCode,
+                  matching:false
+                };
+                if(obj.testCases.testCases[0].stdout == body.stdout){
+                  resultObj.matching=true;
+                  console.log("Test 0 pass:");
+                }
+                console.log("resulta : "+  JSON.stringify(resultObj));
+                //send to queue obj.testCases.testCases[0].stdout
+              });
+            },
+            function() {
+              options.json.stdin=obj.testCases.testCases[1].stdin;
+              request(options, function(err, res, body) {
+                var resultObj = {
+                  error: err,
+                  response: body,
+                  status: res.statusCode,
+                  matching:false
+                };
+                if(obj.testCases.testCases[1].stdout == body.stdout){
+                  resultObj.matching=true;
+                  console.log("Test 1 pass:");
+                }
+                console.log("resultb : "+ JSON.stringify(resultObj));
+                //send to queue obj.testCases.testCases[0].stdout
+              });
+            },
+          function() {
+              options.json.stdin=obj.testCases.testCases[2].stdin;
+              request(options, function(err, res, body) {
+                  var resultObj = {
+                      error: err,
+                      response: body,
+                      status: res.statusCode,
+                      matching:false
+                  };
+                  if(obj.testCases.testCases[2].stdout == body.stdout){
+                      resultObj.matching=true;
+                      console.log("Test 2  pass:");
+                  }
+                  console.log("resultb : "+ JSON.stringify(resultObj));
+                  //send to queue obj.testCases.testCases[0].stdout
+              });
+          }
+          ]);
+          request(options, callback);
 
-      request(options, callback);
     }, {noAck: true});
   });
 });
