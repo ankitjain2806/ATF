9780fd852bb5726b58e36187bdde1909792d8ba8
Krishna <krishna.parachikapu@accolite.com>
2018-02-22 19:34:19 +0530

    added debug statements.

--- build.sh
+++ build.sh
@@ -12,7 +12,6 @@ cd server
 echo "Updating server npm"
 npm install
 export NODE_ENV=development
-echo "Starting the server"
 #npm start
 #nohup  npm start & > /srv/logs/atf/log1.js
 export PID_DIR=/opt/live/apps/ATF
@@ -20,13 +19,20 @@ export PID_SERVER_FILE=$PID_DIR/atf-dev.pid
 export PID_COMPILER_FILE=$PID_DIR/atf-compiler.pid
 export LOG_PATH=/opt/live/apps/ATF/logs
 export LOG_FILE=$LOG_DIR/server.log
+echo "Stopping server"
 forever stop ./bin/www
+echo "Stopping server done!!!"
+echo "Starting the server"
 forever start -o $LOG_PATH/server_out.log -e $LOG_PATH/server_err.log ./bin/www
+echo "Starting the server done!!!"
 # forever start -o /opt/live/apps/ATF/logs/server_out.log -e /opt/live/apps/ATF/logs/server_err.log ./bin/www
 
 cd microservices
-echo "Starting microservice"
+echo "Stopping microservice"
 forever stop compiler.js
+echo "Stopping microservice done!!!"
+echo "Starting microservice"
 forever start -o $LOG_PATH/compiler_out.log -e $LOG_PATH/compiler_err.log compiler.js
+echo "Starting microservice done!!!"
 
 
