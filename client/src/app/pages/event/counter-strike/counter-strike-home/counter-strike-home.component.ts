c9e7c6838f02c5813bdfe05aa074976e4e468218
divyanshu-18 <divyanshuceg@gmail.com>
2018-03-09 17:53:46 +0530

    Added Counter Strike Registration Page.

---
+++ client/src/app/pages/event/counter-strike/counter-strike-home/counter-strike-home.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  selector : "<couter-strike-home>",
+  templateUrl : "./counter-strike-home.html"
+})
+
+export class CounterStrikeHomeComponent implements OnInit{
+  constructor(){
+
+  }
+
+  ngOnInit(){
+  }
+}
---
+++ client/src/app/pages/event/counter-strike/counter-strike-registration/counter-strike-registration.component.ts
@@ -0,0 +1,52 @@
+import {Component, OnInit} from "@angular/core";
+import {FormArray, FormBuilder, FormGroup} from "@angular/forms";
+import {EventService} from "../../event.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector : "<counter-strike-registration>",
+  templateUrl : "./counter-strike-registration.component.html"
+})
+
+export class CounterStrikeRegistrationComponent implements OnInit{
+
+  registrationForm : FormGroup;
+
+  constructor(private _fb: FormBuilder, private eventService:EventService,
+              private router: Router){
+  }
+
+  ngOnInit(){
+      this.registrationForm = this._fb.group({
+        slug: 'counterStrike',
+        members : this._fb.array([this.initEmail()]),
+        teamName: ''
+      });
+    }
+
+  initEmail() {
+    return this._fb.group({
+      email : ['']
+    });
+  }
+
+  gotoHome(){
+    this.router.navigate(['event/counterstrike/home']);
+  }
+
+  onSubmit(){
+    this.eventService.completeHackathonRegistration(this.registrationForm.value).subscribe(data=>{
+      this.gotoHome();
+    });
+  }
+
+  addMembers(){
+    const control = <FormArray>this.registrationForm.controls['members'];
+    control.push(this.initEmail());
+  }
+
+  removeMembers(index: number){
+    const control = <FormArray>this.registrationForm.controls['members'];
+    control.removeAt(index);
+  }
+}
