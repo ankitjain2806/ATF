131a219b643de5a40a0bada63d6eb01143a5ea6d
Akash Yadav <akash.yadav@accoliteindia.com>
2018-03-12 00:13:22 +0530

    Adding async for updating points in transaction

--- server/microservices/transaction.js
+++ server/microservices/transaction.js
@@ -3,6 +3,7 @@ var app = express()
 var amqp = require('amqplib/callback_api');
 var User = require('../models/User');
 var TransactionLog = require('../models/TransactionLog');
+var async = require('async');
 var request = require('request');
 var db = require('../config/db_connect');
 amqp.connect('amqp://localhost:5672', function (err, conn) {
@@ -24,45 +25,63 @@ amqp.connect('amqp://localhost:5672', function (err, conn) {
 
 			// transactionData = JSON.parse(transactionData);
 			var amount = transactionData.points;
-			User.findById(transactionData.user, function (err, person) {
-				console.log("PERSON is :" + person._id);
-				if (err) {
-					callback(err, null)
-				}
-				if (person) {
-					console.log("inside update points", person.totalPoints);
-					person.totalPoints += amount;
-					console.log("inside update points again ", person.totalPoints);
-					person.save(function (err, data) {
+			
+			async.series([
+						function(callback) {
+							User.findById(transactionData.user, function (err, person) {
+								console.log("PERSON is :" + person._id);
+								if (err) {
+									callback(err, null)
+								}
+								if (person) {
+									console.log("inside update points", person.totalPoints);
+									person.totalPoints += amount;
+									console.log("inside update points again ", person.totalPoints);
+									person.save(function (err, data) {
+										callback(err,data);
+									})
+								}
+							});
+						
+						},
+						function(callback) {
+							if (transactionData.fromUser !== null) {
+								User.findById( transactionData.fromUser, function (err, person) {
+									console.trace(err, person)
+									if (err) {
+										callback(err, null)
+									}
+									if (person) {
+										person.totalPoints -= amount;
+										person.save(function (err, data) {
+											callback(err, data);
+										})
+									}
+								});
+							}
+							else{
+								callback();
+							}
+						},
+						function(callback){
+							var transData = {
+								userId: transactionData.toUser,
+								fromUser: transactionData.fromUser,
+								eventId: transactionData.eventId,
+								description: transactionData.description,
+								points: transactionData.amount
+							}
+							var transaction = new TransactionLog(transData);
+							transaction.save(function (err, data) {
+								console.log(err, data)
+								callback(err,data);
+								//ch.ack(msg);
+							})
+					}
+					],
+					function(err, results) {
 						ch.ack(msg);
-					})
-				}
-			});
-			if (transactionData.fromUser !== null) {
-				User.findOne({_id: transactionData.fromUser}, function (err, person) {
-					console.trace(err, person)
-					if (err) {
-						callback(err, null)
-					}
-					if (person) {
-						person.totalPoints -= amount;
-						person.save(function (err, data) {
-						})
-					}
-				});
-			}
-			var transData = {
-				userId: transactionData.toUser,
-				fromUser: transactionData.fromUser,
-				eventId: transactionData.eventId,
-				description: transactionData.description,
-				points: transactionData.amount
-			}
-			var transaction = new TransactionLog(transData);
-			transaction.save(function (err, data) {
-				console.log(err, data)
-				//ch.ack(msg);
-			})
+					});
 		}, {noAck: false});
 	});
 });
