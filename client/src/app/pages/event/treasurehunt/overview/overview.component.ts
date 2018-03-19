2c16da3bdb49a4d665da68f6decbea5b4f736149
divyanshu-18 <divyanshuceg@gmail.com>
2018-03-19 13:05:20 +0530

    Added Loader Service in overview.component.ts

--- client/src/app/pages/event/treasurehunt/overview/overview.component.ts
+++ client/src/app/pages/event/treasurehunt/overview/overview.component.ts
@@ -3,6 +3,7 @@ import {HttpService} from "../../../../shared/util/http.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {UserSessionService} from "../../../../shared/util/user-session.service";
 import {TreasurehuntService} from "../treasurehunt.service";
+import {LoaderService} from "../../../../shared/util/loader.service";
 
 @Component({
   selector: 'app-overview',
@@ -20,19 +21,24 @@ export class OverviewComponent implements OnInit, OnDestroy {
   constructor(private session: UserSessionService,
               private router: Router,
               private activatedRouter: ActivatedRoute,
-              private treasurehuntService: TreasurehuntService) {
+              private treasurehuntService: TreasurehuntService,
+              private loaderService: LoaderService) {
     this.userSession = this.session.getSession();
   }
 
   ngOnInit() {
-
+    this.loaderService.showLoader();
     this.paramsSub = this.activatedRouter.params.subscribe(params => {
       this.eventId = params['id'];
-
+      this.loaderService.hideLoader();
+      this.loaderService.showLoader();
       this.treasurehuntService.getEventInformation().subscribe((data) => {
         this.info = data.data;
+        this.loaderService.hideLoader();
+      },error2 => {
+        this.loaderService.hideLoader();
       });
-
+      this.loaderService.showLoader();
       this.treasurehuntService.getUserStage(this.userSession._id, this.eventId).subscribe((response) => {
         if (response['error']) {
           // user is not present; redirect to home
@@ -40,13 +46,18 @@ export class OverviewComponent implements OnInit, OnDestroy {
           this.isNew = response['data'].length === 0;
           console.log('user is registerd as new ? ', this.isNew);
         }
+        this.loaderService.hideLoader();
+      },error2 => {
+        this.loaderService.hideLoader();
       });
+    },error2 => {
+      this.loaderService.hideLoader();
     });
   }
 
   registerForEvent() {
     // Temp implementation
-
+    this.loaderService.showLoader();
     this.treasurehuntService.setUserState(this.userSession._id,
       {'stage': 0, 'multiplier': 1},
       this.eventId)
@@ -57,6 +68,9 @@ export class OverviewComponent implements OnInit, OnDestroy {
         } else if (response['data']) {
           this.router.navigate(['/treasurehunt', this.eventId, 'game']);
         }
+        this.loaderService.hideLoader();
+      },error2 => {
+        this.loaderService.hideLoader();
       });
   }
 
